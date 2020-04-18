d31c79d45ef48fb1f6518af97f28f1a826e9ac8a
Daniel <danielcu.sanchez@gmail.com>
2020-04-17 20:25:50 -0500

    Webpack configuration plugins

---
+++ plugins/src/js/form.js
@@ -0,0 +1,4 @@
+import '../css/style.css'
+let text = "Hola! form con css "
+console.log(text)
+document.body.innerHTML = `<p>${text}</p>`
---
+++ plugins/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: {
+        form: path.resolve(__dirname, 'src/js/form.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCSSExtractPlugin.loader
+                    },
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Plugins'
+        }),
+        new MiniCSSExtractPlugin({
+            filename: 'css/[name].css'
+        })
+    ]
+};
